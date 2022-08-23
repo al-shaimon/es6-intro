1f8a7d845b04807640b1ca259f347725088a0adc
al-shaimon <alshaimon968@gmail.com>
2022-08-24 02:06:14 +0600

    first commit

---
+++ js/let-const.js
@@ -0,0 +1,13 @@
+// breakup with var
+// no more use of var
+// let: let it to reassign
+// const: do not allow it to reassign
+
+let money = 26;
+money = 35;
+console.log(money);
+
+const bird = "jaan pakhi";
+// bird = 'moyna pakhi';
+const message = bird + "potas potas";
+console.log(message);
